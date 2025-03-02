2bf770af16823a317e121019f19f7db2820a4b81
LeeJeongGyun <jeonggyun0630@gmail.com>
2025-03-02 15:04:14 +0900

    Connect 하는 로직을 담당하는 클래스 구현

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -3,6 +3,38 @@
     using System.Net;
     using System.Net.Sockets;
     using System.Text;
+    using ServerCore;
+
+    internal class ClientSession : Session
+    {
+        /// <inheritdoc/>
+        public override void OnConnected(EndPoint endPoint)
+        {
+            for (int i = 0; i < 5; ++i)
+            {
+                byte[] sendBuf = Encoding.UTF8.GetBytes($"Hello World {i + 1}");
+                Send(sendBuf);
+            }
+        }
+
+        /// <inheritdoc/>
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine("[Client] OnDisconnected");
+        }
+
+        /// <inheritdoc/>
+        public override void OnRecv(ArraySegment<byte> recvData)
+        {
+            Console.WriteLine($"[CLIENT] RecvData: {Encoding.UTF8.GetString(recvData)}");
+        }
+
+        /// <inheritdoc/>
+        public override void OnSend(int byteOfTransferred)
+        {
+            Console.WriteLine($"Transferred Data: {byteOfTransferred}");
+        }
+    }
 
     internal class Program
     {
@@ -15,26 +47,9 @@
 
             while (true)
             {
-                try
-                {
-                    Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                    socket.Connect(endPoint);
-                    Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
-
-                    byte[] sendBuf = Encoding.UTF8.GetBytes("You Server?");
-                    socket.Send(sendBuf);
-
-                    byte[] recvBuf = new byte[1024];
-                    socket.Receive(recvBuf);
-                    Console.WriteLine(Encoding.UTF8.GetString(recvBuf));
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                    throw;
-                }
-
-                Thread.Sleep(100);
+                Connector connector = new Connector();
+                connector.Connect(endPoint, () => { return new ClientSession(); });
+                Thread.Sleep(2000);
             }
         }
     }
---
+++ ServerCore/Connector.cs
@@ -0,0 +1,55 @@
+﻿namespace ServerCore;
+
+using System.Net;
+using System.Net.Sockets;
+
+public class Connector
+{
+    private Func<Session> _sessionFactory = null!;
+
+    /// <summary>
+    /// 인자로 들어온 endPoint와 Connect하는 함수
+    /// </summary>
+    /// <param name="endPoint">연결을 원하는 Peer.</param>
+    /// <param name="sessionFactory">Session 생성 팩토리.</param>
+    /// <remarks>
+    ///     여러 EndPoint와 연결을 할 수 있기 때문에 인자로 소켓을 가지고 있지 않고, Connect 함수에서 소켓을 생성한 후 UserToken으로 전달.
+    ///     RemoteEndPoint 세팅이 필요하다.
+    /// </remarks>
+    public void Connect(IPEndPoint endPoint, Func<Session> sessionFactory)
+    {
+        _sessionFactory = sessionFactory;
+        Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+        SocketAsyncEventArgs connectArgs = new SocketAsyncEventArgs();
+        connectArgs.Completed += OnCompletedConnect;
+        connectArgs.RemoteEndPoint = endPoint;
+        connectArgs.UserToken = socket;
+        RegisterConnect(connectArgs);
+    }
+
+    private void OnCompletedConnect(object? sender, SocketAsyncEventArgs args)
+    {
+        if (args.SocketError == SocketError.Success)
+        {
+            Session session = _sessionFactory.Invoke();
+            session.Start(args.ConnectSocket);
+            session.OnConnected(args.RemoteEndPoint);
+        }
+        else
+        {
+            Console.WriteLine("OnCompletedConnect Error");
+        }
+    }
+
+    private void RegisterConnect(SocketAsyncEventArgs args)
+    {
+        Socket socket = args.UserToken as Socket;
+        if (socket == null)
+            return;
+
+        bool pending = socket.ConnectAsync(args);
+        if (!pending)
+            OnCompletedConnect(null, args);
+    }
+}
